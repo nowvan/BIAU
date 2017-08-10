096e3848ead7209623db3de7de678ed7056e3ed1
PercomLab <percomlab@PercomLabdeMacBook-Air.local>
2017-08-10 12:14:44 +0800

    與ＩＰＦＳ結合後的ＧＷ們

---
+++ GW2-ipfs/app.js
@@ -0,0 +1,154 @@
+/*jshint esversion: 6 */
+/* eslint-env mocha */
+/* eslint max-nested-callbacks: ["error", 8] */
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var Web3 = require('web3');
+var request = require('request');
+var router = express.Router();
+const IPFS = require('ipfs');
+const os = require('os');
+var chai = require('chai');
+var expect = chai.expect;
+var assert = chai.assert;
+const dirtyChai = require('dirty-chai');
+chai.use(dirtyChai);
+const bs58 = require('bs58');
+const Readable = require('readable-stream');
+const loadFixture = require('aegir/fixtures');
+const bl = require('bl');
+const isNode = require('detect-node');
+const concat = require('concat-stream');
+const through = require('through2');
+const Buffer = require('safe-buffer').Buffer;
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/users', users);
+
+const ethereumUri = 'http://localhost:8545';
+var web3 = new Web3();
+var eth = web3.eth;
+web3.setProvider(new web3.providers.HttpProvider(ethereumUri));
+var company = "van";
+var fileUrl;
+var fileName;
+
+var node;
+
+
+
+//////////////////////events////////////////////
+var producecontract = web3.eth.contract(
+		[{"constant":true,"inputs":[{"name":"company","type":"string"}],"name":"getFileInfo","outputs":[{"name":"filename","type":"string"},{"name":"url","type":"string"},{"name":"newest","type":"string"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"BIAU","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"company","type":"string"},{"name":"filename","type":"string"},{"name":"url","type":"string"},{"name":"newest","type":"string"}],"name":"putFileInfo","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"isBIAU","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"inputs":[],"payable":false,"type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"company","type":"string"},{"indexed":false,"name":"filename","type":"string"},{"indexed":false,"name":"url","type":"string"},{"indexed":false,"name":"newest","type":"string"}],"name":"fileUploadEvent","type":"event"}]
+		).at("0xd12d45cab9d5b7a045665dd0a92c7174d436fead");
+///////////afterNodeInit/////////////////
+function handleInit (err) {
+	  if (err) {
+	    throw err;
+	  }
+	  
+	  
+	  setTimeout(function(){
+		  node.start(() => {
+			  console.log('Online status: ', node.isOnline() ? 'online' : 'offline');
+	
+			  //document.getElementById("status").innerHTML= 'Node status: ' + (node.isOnline() ? 'online' : 'offline');
+		  
+		  
+			  //const hash = 'QmZSm2NANrdEREwhesUwU9htLdg9PZXr7tSSguBHz1ZdXR';
+			  //const hash = 'QmTuneUZdjb9RA2oLHG333NtjAi9WoEPxssnarTWBFwFf8';
+			  const hash = fileUrl;
+			  //console.log(hash);
+			  node.files.get(hash, (err, stream) => {
+	    	  	  	expect(err).to.not.exist();
+	    	  	  	let files = [];
+	    	  		stream.pipe(through.obj((file, enc, next) => {
+	    	  			file.content.pipe(concat((content) => {
+	    	  				files.push({
+	    	  					path: file.path,
+	    	  					content: content
+	    	  				});
+	    	  				next();
+	    	  			}));
+	    	  		}, () => {
+	    	  				expect(files).to.be.length(1);
+	    	  				expect(files[0].path).to.be.eql(hash);
+	    	  				var output = files[0].content;
+	    	  				//console.log(output);
+	    	  				fs.writeFile("../public/"+fileName, output, function(err) {
+	    	  					if(err) {
+	    	  						return console.log(err);
+	    	  					}
+	    	  					console.log("The file was saved!");
+	    	  				}); 
+	    	  			}));
+			  });//endGet
+	  	});  
+	  },3000);//endsettime
+}
+//當 smartcontract 的 setEvent 被發射的時候，就會被如此接住
+producecontract.fileUploadEvent({}, function(err, eventdata) {
+	////////IPFS//////////
+	console.log('start');
+	const repoPath = 'ipfs-' + Math.random();
+	console.log(repoPath);
+	// Create an IPFS node
+	node = new IPFS({
+	  init: true,
+	  start: true,
+	  repo: repoPath
+	 
+	});	
+	console.log('dataSet fired : ');
+	console.log(eventdata.args);
+	if(company === eventdata.args.company){
+		console.log("START DONLOAD");
+		fileUrl  = eventdata.args.url;
+		fileName = eventdata.args.filename;
+		//初始化node
+		node.init(handleInit);	
+	}
+});//end event uploadfile
+
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
